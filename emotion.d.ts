08db7f76a222d0140d2d34c7a05498be58354101
Jooeun-K <gangjoo93@gmail.com>
2023-06-24 23:19:13 +0900

    style: lint fix를 이용하여 기존 작성 스타일 코드의 포맷팅

--- emotion.d.ts
+++ emotion.d.ts
@@ -1,8 +1,8 @@
-import "@emotion/react";
-import theme from "./styles/theme";
+import '@emotion/react'
+import theme from './styles/theme'
 
-type ThemeType = typeof theme;
+type ThemeType = typeof theme
 
-declare module "@emotion/react" {
+declare module '@emotion/react' {
   export interface Theme extends ThemeType {}
 }
