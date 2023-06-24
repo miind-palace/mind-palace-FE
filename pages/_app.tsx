0a7afeab32ff9da743e15f01587bf0cecb119664
Ycow <skypnal12@gmail.com>
2023-06-24 19:11:25 +0900

    feat: reset css & theme, theme 타입추론 설정[#4]

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,13 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import globalStyles from "@/styles/globalStyles";
+import theme from "@/styles/theme";
+import { Global, ThemeProvider } from "@emotion/react";
+import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={theme}>
+      <Global styles={globalStyles} />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
