42332d9e302d1e37bb961c7bc083ab30e21a6289
Jooeun-K <gangjoo93@gmail.com>
2023-06-25 18:38:37 +0900

    feat: 분해 재조립되는 큐브 애니메이션 작성

---
+++ components/Intro/BounceCube/BounceCube.tsx
@@ -0,0 +1,248 @@
+import { BounceCubeWrapper } from './BounceCube.styles'
+
+const BounceCube = () => {
+  return (
+    <BounceCubeWrapper>
+      <div className="assembly">
+        <div className="comp-3d comp-3d--i">
+          <div className="cube">
+            <div className="cube__face"></div>
+            <div className="cube__face"></div>
+            <div className="cube__face"></div>
+            <div className="cube__face"></div>
+            <div className="cube__face"></div>
+            <div className="cube__face"></div>
+          </div>
+          <div className="cube">
+            <div className="cube__face"></div>
+            <div className="cube__face"></div>
+            <div className="cube__face"></div>
+            <div className="cube__face"></div>
+            <div className="cube__face"></div>
+            <div className="cube__face"></div>
+          </div>
+          <div className="cube">
+            <div className="cube__face"></div>
+            <div className="cube__face"></div>
+            <div className="cube__face"></div>
+            <div className="cube__face"></div>
+            <div className="cube__face"></div>
+            <div className="cube__face"></div>
+          </div>
+          <div className="cube">
+            <div className="cube__face"></div>
+            <div className="cube__face"></div>
+            <div className="cube__face"></div>
+            <div className="cube__face"></div>
+            <div className="cube__face"></div>
+            <div className="cube__face"></div>
+          </div>
+          <div className="cube">
+            <div className="cube__face"></div>
+            <div className="cube__face"></div>
+            <div className="cube__face"></div>
+            <div className="cube__face"></div>
+            <div className="cube__face"></div>
+            <div className="cube__face"></div>
+          </div>
+          <div className="cube">
+            <div className="cube__face"></div>
+            <div className="cube__face"></div>
+            <div className="cube__face"></div>
+            <div className="cube__face"></div>
+            <div className="cube__face"></div>
+            <div className="cube__face"></div>
+          </div>
+          <div className="cube">
+            <div className="cube__face"></div>
+            <div className="cube__face"></div>
+            <div className="cube__face"></div>
+            <div className="cube__face"></div>
+            <div className="cube__face"></div>
+            <div className="cube__face"></div>
+          </div>
+          <div className="cube">
+            <div className="cube__face"></div>
+            <div className="cube__face"></div>
+            <div className="cube__face"></div>
+            <div className="cube__face"></div>
+            <div className="cube__face"></div>
+            <div className="cube__face"></div>
+          </div>
+          <div className="cube">
+            <div className="cube__face"></div>
+            <div className="cube__face"></div>
+            <div className="cube__face"></div>
+            <div className="cube__face"></div>
+            <div className="cube__face"></div>
+            <div className="cube__face"></div>
+          </div>
+          <div className="cube">
+            <div className="cube__face"></div>
+            <div className="cube__face"></div>
+            <div className="cube__face"></div>
+            <div className="cube__face"></div>
+            <div className="cube__face"></div>
+            <div className="cube__face"></div>
+          </div>
+          <div className="cube">
+            <div className="cube__face"></div>
+            <div className="cube__face"></div>
+            <div className="cube__face"></div>
+            <div className="cube__face"></div>
+            <div className="cube__face"></div>
+            <div className="cube__face"></div>
+          </div>
+          <div className="cube">
+            <div className="cube__face"></div>
+            <div className="cube__face"></div>
+            <div className="cube__face"></div>
+            <div className="cube__face"></div>
+            <div className="cube__face"></div>
+            <div className="cube__face"></div>
+          </div>
+          <div className="cube">
+            <div className="cube__face"></div>
+            <div className="cube__face"></div>
+            <div className="cube__face"></div>
+            <div className="cube__face"></div>
+            <div className="cube__face"></div>
+            <div className="cube__face"></div>
+          </div>
+          <div className="cube">
+            <div className="cube__face"></div>
+            <div className="cube__face"></div>
+            <div className="cube__face"></div>
+            <div className="cube__face"></div>
+            <div className="cube__face"></div>
+            <div className="cube__face"></div>
+          </div>
+          <div className="cube">
+            <div className="cube__face"></div>
+            <div className="cube__face"></div>
+            <div className="cube__face"></div>
+            <div className="cube__face"></div>
+            <div className="cube__face"></div>
+            <div className="cube__face"></div>
+          </div>
+          <div className="cube">
+            <div className="cube__face"></div>
+            <div className="cube__face"></div>
+            <div className="cube__face"></div>
+            <div className="cube__face"></div>
+            <div className="cube__face"></div>
+            <div className="cube__face"></div>
+          </div>
+          <div className="cube">
+            <div className="cube__face"></div>
+            <div className="cube__face"></div>
+            <div className="cube__face"></div>
+            <div className="cube__face"></div>
+            <div className="cube__face"></div>
+            <div className="cube__face"></div>
+          </div>
+          <div className="cube">
+            <div className="cube__face"></div>
+            <div className="cube__face"></div>
+            <div className="cube__face"></div>
+            <div className="cube__face"></div>
+            <div className="cube__face"></div>
+            <div className="cube__face"></div>
+          </div>
+          <div className="cube">
+            <div className="cube__face"></div>
+            <div className="cube__face"></div>
+            <div className="cube__face"></div>
+            <div className="cube__face"></div>
+            <div className="cube__face"></div>
+            <div className="cube__face"></div>
+          </div>
+        </div>
+        <div className="comp-3d comp-3d--o">
+          <div className="pos">
+            <div className="cube">
+              <div className="cube__face"></div>
+              <div className="cube__face"></div>
+              <div className="cube__face"></div>
+              <div className="cube__face"></div>
+              <div className="cube__face"></div>
+              <div className="cube__face"></div>
+            </div>
+          </div>
+          <div className="pos">
+            <div className="cube">
+              <div className="cube__face"></div>
+              <div className="cube__face"></div>
+              <div className="cube__face"></div>
+              <div className="cube__face"></div>
+              <div className="cube__face"></div>
+              <div className="cube__face"></div>
+            </div>
+          </div>
+          <div className="pos">
+            <div className="cube">
+              <div className="cube__face"></div>
+              <div className="cube__face"></div>
+              <div className="cube__face"></div>
+              <div className="cube__face"></div>
+              <div className="cube__face"></div>
+              <div className="cube__face"></div>
+            </div>
+          </div>
+          <div className="pos">
+            <div className="cube">
+              <div className="cube__face"></div>
+              <div className="cube__face"></div>
+              <div className="cube__face"></div>
+              <div className="cube__face"></div>
+              <div className="cube__face"></div>
+              <div className="cube__face"></div>
+            </div>
+          </div>
+          <div className="pos">
+            <div className="cube">
+              <div className="cube__face"></div>
+              <div className="cube__face"></div>
+              <div className="cube__face"></div>
+              <div className="cube__face"></div>
+              <div className="cube__face"></div>
+              <div className="cube__face"></div>
+            </div>
+          </div>
+          <div className="pos">
+            <div className="cube">
+              <div className="cube__face"></div>
+              <div className="cube__face"></div>
+              <div className="cube__face"></div>
+              <div className="cube__face"></div>
+              <div className="cube__face"></div>
+              <div className="cube__face"></div>
+            </div>
+          </div>
+          <div className="pos">
+            <div className="cube">
+              <div className="cube__face"></div>
+              <div className="cube__face"></div>
+              <div className="cube__face"></div>
+              <div className="cube__face"></div>
+              <div className="cube__face"></div>
+              <div className="cube__face"></div>
+            </div>
+          </div>
+          <div className="pos">
+            <div className="cube">
+              <div className="cube__face"></div>
+              <div className="cube__face"></div>
+              <div className="cube__face"></div>
+              <div className="cube__face"></div>
+              <div className="cube__face"></div>
+              <div className="cube__face"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </BounceCubeWrapper>
+  )
+}
+
+export default BounceCube
