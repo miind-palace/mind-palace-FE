be348ed14d4e16aeb1e5321055aff70f5da7f039
은배 <geb9211@gmail.com>
2023-06-26 22:24:28 +0900

    feat: url에 youtube가 없다면 빈 string 반환하도록 수정

--- lib/utils/makeYouTubeVideoId.ts
+++ lib/utils/makeYouTubeVideoId.ts
@@ -1,11 +1,11 @@
-const sliceYouTubeUrlType = (youtubeUrl: string) => youtubeUrl.slice()
-
 const isUrlContainsVParam = (youtubeUrl: string) => {
   const regex = /\?v=/
   return regex.test(youtubeUrl)
 }
 
 const makeYouTubeVideoId = (youtubeUrl: string) => {
+  if (!youtubeUrl.includes('youtu.be/') && !youtubeUrl.includes('youtube.com')) return ''
+
   if (isUrlContainsVParam(youtubeUrl)) return new URL(youtubeUrl).searchParams.get('v') as string
   else return youtubeUrl.slice(youtubeUrl.lastIndexOf('/') + 1)
 }
