2aec0c2f32081228469ffd13609dadab6f4a558b
Jooeun-K <gangjoo93@gmail.com>
2023-06-26 22:22:49 +0900

    fix: env 이슈 확인

--- next.config.js
+++ next.config.js
@@ -1,14 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // async rewrites() {
-  //   return [
-  //     {
-  //       source: '/:path*',
-  //       destination: `${process.env.NEXT_PUBLIC_SERVER_DEFAULT_END_POINT}/:path*`,
-  //     },
-  //   ]
-  // },
+  async rewrites() {
+    return [
+      {
+        source: '/:path*',
+        destination: `${process.env.NEXT_PUBLIC_SERVER_DEFAULT_END_POINT}/:path*`,
+      },
+    ]
+  },
   async redirects() {
     return [
       {
