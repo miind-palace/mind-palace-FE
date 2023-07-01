69b07de6827a38ca263232a2865200df49d1e3bf
Ycow <skypnal12@gmail.com>
2023-07-01 21:47:10 +0900

    feat: useChangePreviewImage 훅 개선, 불필요 state 제거, 생성한 URL 메모리에 남지 않도록 제거 가독성 확보[#59]

--- hooks/useChangePreviewImage.ts
+++ hooks/useChangePreviewImage.ts
@@ -1,39 +1,34 @@
+import { ImagesTypes } from '@/lib/hooks/useCreateSuggestionImage'
 import { useState, useEffect } from 'react'
 
 export const useChangePreviewImage = () => {
   const [imgFile, setImgFile] = useState<File | null>()
-  const [preview, setPreview] = useState<string | null>('')
-  const [hasImage, setHasImage] = useState(false)
+  const [preview, setPreview] = useState('')
 
   const onChangeBackgroundImage = (event: React.ChangeEvent<HTMLInputElement>) => {
-    if (event.target.files !== null) {
-      const file = event.target.files[0]
-      if (file && file.type.substring(0, 5) === 'image') {
-        setImgFile(file)
-        setHasImage(true)
-      } else {
-        setImgFile(null)
-        setHasImage(false)
+    if (event.target.files) {
+      // 이전 생성한 프리뷰 URL 제거 (메모리 확보)
+      if (imgFile) {
+        URL.revokeObjectURL(preview)
       }
+
+      const file = event.target.files[0]
+
+      setImgFile(file)
+      setPreview(URL.createObjectURL(file))
     }
   }
 
-  useEffect(() => {
-    if (imgFile) {
-      const reader = new FileReader()
-      reader.onloadend = () => {
-        setPreview(reader.result as string)
-      }
-      reader.readAsDataURL(imgFile)
-    }
-  }, [imgFile])
+  const onClickSuggestionImage = ([file, imageUrl]: ImagesTypes) => {
+    setImgFile(file)
+    setPreview(imageUrl)
+  }
 
   return {
     preview,
     onChangeBackgroundImage,
+    onClickSuggestionImage,
     setPreview,
-    setHasImage,
-    hasImage,
     setImgFile,
     imgFile,
   }
