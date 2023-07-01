c6e58c8fe3a226c0b332143ba024c7e5e8b328b8
Ycow <skypnal12@gmail.com>
2023-07-01 21:41:30 +0900

    refactor: 기존 이미지 휠 이벤트 제거하고 debounce 적용, onWheel 이벤트로 변경 및 wrapper 기준으로 이동값 변경하도록 수정[#59]

---
+++ hooks/useHorizontalWheel.ts
@@ -0,0 +1,20 @@
+import { debounce } from 'lodash'
+import { WheelEvent, useRef, useCallback, useMemo } from 'react'
+
+const useHorizontalWheel = () => {
+  const wheelRef = useRef<null | HTMLDivElement>(null)
+  const onWheelHandler = useMemo(
+    () =>
+      debounce((e: WheelEvent<HTMLDivElement>) => {
+        if (wheelRef.current) {
+          wheelRef.current.scrollLeft +=
+            e.deltaY > 0 ? wheelRef.current.clientWidth + 10 : -wheelRef.current.clientWidth + -10
+        }
+      }, 100),
+    []
+  )
+
+  return { wheelRef, onWheelHandler }
+}
+
+export default useHorizontalWheel
