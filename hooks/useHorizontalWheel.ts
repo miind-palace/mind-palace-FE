9e37efbbc1d2aa551f268357907b026132dea669
Ycow <skypnal12@gmail.com>
2023-07-02 20:01:28 +0900

    refactor: 이미지 생성 훅 에러 state 제거, 휠 이벤트 훅 useMemo제거, lodash import 수정[#59]

--- hooks/useHorizontalWheel.ts
+++ hooks/useHorizontalWheel.ts
@@ -1,18 +1,14 @@
-import { debounce } from 'lodash'
-import { WheelEvent, useMemo, useRef } from 'react'
+import debounce from 'lodash/debounce'
+import { WheelEvent, useRef } from 'react'
 
 const useHorizontalWheel = () => {
   const wheelRef = useRef<null | HTMLDivElement>(null)
-  const onWheelHandler = useMemo(
-    () =>
-      debounce((e: WheelEvent<HTMLDivElement>) => {
-        if (wheelRef.current) {
-          wheelRef.current.scrollLeft +=
-            e.deltaY > 0 ? wheelRef.current.clientWidth + 10 : -wheelRef.current.clientWidth + -10
-        }
-      }, 100),
-    []
-  )
+  const onWheelHandler = debounce((e: WheelEvent<HTMLDivElement>) => {
+    if (wheelRef.current) {
+      wheelRef.current.scrollLeft +=
+        e.deltaY > 0 ? wheelRef.current.clientWidth + 10 : -wheelRef.current.clientWidth + -10
+    }
+  }, 100)
 
   return { wheelRef, onWheelHandler }
 }
