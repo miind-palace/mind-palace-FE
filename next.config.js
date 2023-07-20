7b0a822a145c23eee5b0b1e3ddd1573b2c51d38d
LeeJiHyeon <rachel2148072@gmail.com>
2023-07-20 23:45:25 +0900

    feat: domain에서 remotePatterns 으로 변경

--- next.config.js
+++ next.config.js
@@ -21,7 +21,14 @@ const nextConfig = {
     ]
   },
   images: {
-    domains: ['res.cloudinary.com'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
   },
 }
 
