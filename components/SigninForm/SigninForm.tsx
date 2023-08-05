bdf1126d44c03963b3b8f8ff9455291b25e30821
Jooeun-K <gangjoo93@gmail.com>
2023-08-05 21:07:27 +0900

    feat: axios 공통 설정을 포함한 axiosHttp instance 작성

--- components/SigninForm/SigninForm.tsx
+++ components/SigninForm/SigninForm.tsx
@@ -1,12 +1,12 @@
 import { useState, FormEvent, ChangeEvent, MouseEvent, useEffect } from 'react'
 import { useRouter } from 'next/router'
-import axios from 'axios'
 import styled from '@emotion/styled'
 import { SecurityIcon } from '../Icons'
 import Input from '@/components/common/Input/Input'
 import Link from 'next/link'
 import BasicButton from '../common/Button/BasicButton'
 import Spacing from '../common/Spacing/Spacing'
+import { axiosHttp } from '@/lib/utils/httpCore'
 
 export default function SigninForm() {
   const router = useRouter()
@@ -36,13 +36,18 @@ export default function SigninForm() {
     // 로그인 버튼 클릭시 폼 제출 후 업로드 페이지로 라우팅
     e.preventDefault()
 
+    debugger
     if (!signinConditions.email || !signinConditions.password) return alert('아이디나 비밀번호를 입력하세요!')
 
     try {
-      const response = await axios.post(`${process.env.NEXT_PUBLIC_SERVER_DEFAULT_END_POINT}member/login`, {
+      const response = await axiosHttp.post(`/member/login`, {
         memberEmail: signinConditions.email,
         memberPassword: signinConditions.password,
       })
+
+      console.log(response)
+      console.log(response.headers['set-cookie'])
+      debugger
 
       if (response.data === '로그인 실패') return alert(response.data)
 
