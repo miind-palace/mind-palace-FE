9046d5a216b0ea08e621fa71997b7e2bc7a8465c
brother1-4752 <guddlf4752@gmail.com>
2023-08-16 03:09:36 +0900

    style: 카메라, 빛, 메인룸, 타겟 포지션 조정

--- pages/main/index.tsx
+++ pages/main/index.tsx
@@ -2,8 +2,8 @@ import { OrbitControls } from '@react-three/drei'
 import { Canvas } from '@react-three/fiber'
 import styled from '@emotion/styled'
 
-import CustomSuspense from '@/components/common/Suspense/CustomSuspense'
 import CubeLoader from '@/components/common/Loader/CubeLoader'
+import CustomSuspense from '@/components/common/Suspense/CustomSuspense'
 import MainRoom from '@/components/main/MainRoom'
 
 export default function Main() {
@@ -17,10 +17,10 @@ export default function Main() {
         }
         maxDuration={5500}
       >
-        <Canvas camera={{ position: [1, 1, 1] }}>
-          <directionalLight position={[7, 7, 7]} />
-          <MainRoom position={[0, 0, 0]} scale={[0.3, 0.3, 0.3]} />
-          <OrbitControls target={[0, 1.5, 0]} />
+        <Canvas camera={{ position: [1, 1.7, 1] }} style={{ height: '100vh' }}>
+          <directionalLight position={[7, 7, 10]} />
+          <MainRoom position={[0, 0.75, 0]} scale={[0.22, 0.22, 0.22]} />
+          <OrbitControls target={[0, 1.4, 0]} />
         </Canvas>
       </CustomSuspense>
     </>
