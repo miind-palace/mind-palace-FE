fae8e88476a398424dd4458270eac2d3b97d42d4
YunCow <skypnal12@gmail.com>
2023-09-24 21:51:40 +0900

    feat: theme 설정 추가

--- styles/theme.ts
+++ styles/theme.ts
@@ -1,6 +1,57 @@
+const colors = {
+  primary: {
+    normal: '#007AFF',
+    bright: '#3395FF',
+    light: '#66AFFF',
+    strong: '#2559F4',
+    heavy: '#1A4DE5',
+    disable: '#D0DDEB',
+  },
+  text: {
+    strong: '#000000',
+    normal: '#171717',
+    medium: '#474747',
+    alternative: '#8A8A8A',
+    assitive: '#C4C4C4',
+    disable: '#DCDCDC',
+    above: '#FFFFFF',
+    error: '#EE0000',
+  },
+  background: {
+    normal: '#F2F2F7',
+    white: '#FFFFFF',
+    tertiary: '#F2F2F7',
+    random: '#E5E5E5',
+  },
+  line: {
+    normal: '#6D7179',
+    alternative: '#F4F4F5',
+  },
+}
+const typography = {
+  size: {
+    text15: '15px',
+    text16: '16px',
+    text18: '18px',
+    text20: '20px',
+    text24: '24px',
+    text26: '26px',
+    text28: '28px',
+    text36: '36px',
+    text40: '40px',
+  },
+  weight: {
+    light: 300,
+    regular: 400,
+    semiBold: 600,
+    bold: 700,
+    extraBold: 800,
+  },
+}
+
 const theme = {
-  mainColor: '#de7474',
-  subColor: '#508c3f',
+  colors,
+  typography,
 }
 
 export default theme
