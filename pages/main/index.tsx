5f41e935505f762d9cef0999970c2535b4750dce
brother1-4752 <guddlf4752@gmail.com>
2023-11-09 17:37:48 +0900

    fix: 미리 렌더링되는 문제 해결

--- pages/main/index.tsx
+++ pages/main/index.tsx
@@ -6,7 +6,9 @@ import dynamic from 'next/dynamic'
 import CubeLoader from '@/components/common/Loader/CubeLoader'
 import CustomSuspense from '@/components/common/Suspense/CustomSuspense'
 
-const MainRoom = dynamic(() => import('../../components/Main/MainRoom'))
+const MainCanvas = dynamic(() => import('../../components/Main/MainCanvas'), {
+  ssr: false,
+})
 
 export default function Main() {
   return (
@@ -18,11 +20,7 @@ export default function Main() {
       }
       maxDuration={5500}
     >
-      <Canvas camera={{ position: [1, 1.7, 1] }} style={{ height: '100vh' }}>
-        <directionalLight position={[7, 7, 10]} />
-        <MainRoom position={[0, 0.75, 0]} scale={[0.22, 0.22, 0.22]} />
-        <OrbitControls target={[0, 1.4, 0]} />
-      </Canvas>
+      <MainCanvas />
     </CustomSuspense>
   )
 }
